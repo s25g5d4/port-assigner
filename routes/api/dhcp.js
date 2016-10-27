e7d2cf0f73797924ad59288ad2487787a4ca0b0d
s25g5d4 <s25g5d4@gmail.com>
2016-10-27 17:05:08 +0800

    working prototype

--- routes/api/dhcp.js
+++ routes/api/dhcp.js
@@ -7,50 +7,91 @@ const { getMACPortIndex } = require('../../models/switch-snmp');
 
 const router = express.Router();
 
-router.post('/discover', function(req, res, next) {
-  if (req.body.giaddr.join('.') === '0.0.0.0') {
+router.post('/:dorm/discover', function(req, res, next) {
+
+  const findSwitches = () => {
+    return SwitchList.findAll({
+      'attributes': ['ip', 'community', 'uplink', 'subnet'],
+      'where': {
+        'dorm': req.params.dorm,
+        'level': -1
+      }
+    });
+  };
+
+  const getPortIndex = switchList => {
+    const findPort = Promise.all( switchList.map(e => getMACPortIndex(e.ip, e.community, req.body.chaddr)) );
+    return findPort.then(portIndex => {
+      let whichSwitch;
+      for (whichSwitch = 0; whichSwitch < portIndex.length; ++whichSwitch) {
+        if (portIndex[whichSwitch] !== switchList[whichSwitch].uplink) break;
+      }
+      return Promise.resolve([switchList[whichSwitch], portIndex[whichSwitch]]);
+    });
+  };
+
+  const getSwitchPort = (switchInfo, portIndex) => {
+    return switchInfo.getSwitchPort({
+      'attributes': ['dorm', 'room', 'plug'],
+      'where': { 'port': portIndex },
+      'raw': true
+    });
+  };
+
+  const getIp = data => {
+    if (data.length === 0) {
+      return Promise.reject(`cannot find switch port`);
+    }
+
+    const dvo = `${data[0].dorm}${data[0].room}-${data[0].plug}`;
+    return sql.query('SELECT `ip`, `dvo` FROM `ip_dvo` WHERE `dvo`=? OR `dvo`=? ORDER BY `dvo` DESC', {
+      'type': sequelize.QueryTypes.SELECT,
+      'replacements': [dvo, `${data[0].dorm}-gateway`]
+    }).then(data => {
+      if (data.length < 2) return Promise.reject(`cannot find ip of ${dvo}`);
+
+      const userIp = data.filter(e => e.dvo === dvo)[0];
+      const gateway = data.filter(e => e.dvo.match(/gateway/))[0];
+      if (!userIp || !gateway) return Promise.reject(`cannot find ip of ${dvo}`);
+
+      return Promise.resolve({'user': userIp.ip, 'gateway': gateway.ip});
+    });
+  };
+
+  const fillResponse = (ip, gateway, mask, lease) => {
+    const responseObject = {};
+    responseObject.yiaddr = ip;
+    responseObject.subnet_mask = mask;
+    responseObject.router = gateway;
+    responseObject.ip_address_lease_time = lease;
+    return responseObject;
+  };
+
+  const respond = resObj => {
     res
       .set('Content-Type', 'Application/json')
-      .status(403)
-      .send('{}')
+      .status(200)
+      .send(JSON.stringify(resObj))
       .end();
+  };
 
-    return;
-  }
+  const switchList = findSwitches();
+  const portIndex = switchList.then(getPortIndex);
+  const switchPort = portIndex.then( data => getSwitchPort(data[0], data[1]) );
+  const ip = switchPort.then(getIp);
 
-  SwitchList.findById(req.body.giaddr.join('.'), { 'attributes': ['ip', 'community'] })
-    .then(switchInfo => {
-      return Promise.all([
-        Promise.resolve(switchInfo),
-        getMACPortIndex(switchInfo.ip, switchInfo.community, req.body.chaddr)
-      ]);
-    })
-    .then(data => {
-      return data[0].getSwitchPort({
-        'attributes': ['dorm', 'room', 'plug'],
-        'where': {
-          'port': data[1]
-        },
-        'raw': true
-      });
-    })
-    .then(data => {
-      const dvo = `${data[0].dorm}${data[0].room}-${data[0].plug}`;
-      return sql.query('SELECT `ip` FROM `ip_dvo` WHERE `dvo`=?', { 'type': sequelize.QueryTypes.SELECT, 'replacements': [dvo] });
-    })
-    .then(clientIp => {
-      const octetIp = clientIp[0].ip.split('.').map(e => parseInt(e, 10));
-      const resObj = JSON.parse(JSON.stringify(req.body));
-      resObj.yiaddr = octetIp;
-      return Promise.resolve(resObj);
-    })
-    .then(resObj => {
-      res
-        .set('Content-Type', 'Application/json')
-        .status(200)
-        .send(JSON.stringify(resObj))
-        .end();
-    })
+  Promise.all([portIndex, ip]).then(data => {
+    const maskDecimal = parseInt(data[0][0].subnet.split('/')[1], 10);
+    const maskBinary = Array(maskDecimal).fill('1').concat(Array(32-maskDecimal).fill('0'));
+    const maskDotted = maskBinary.join('').match(/\d{8}/g).map(e => parseInt(e, 2)).join('.');
+    const userIp = data[1].user;
+    const gateway = data[1].gateway;
+    const lease = 14400; // 4 hours
+
+    const resObj = fillResponse(userIp, gateway, maskDotted, lease);
+
+    respond(resObj);
+  })
     .catch(err => {
       console.log(err);
       res
@@ -63,17 +104,101 @@ router.post('/discover', function(req, res, next) {
     });
 });
 
-router.post('/request', function(req, res, next) {
-  const log = require('fs').createWriteStream(Date.now() + '.log');
-  log.write(JSON.stringify(req.body, undefined, 2));
+router.post('/:dorm/request', function(req, res, next) {
 
-  getMACPortIndex(req.body.giaddr.join('.'), 'stiks', req.body.chaddr).then(data => console.log(data)).catch(err => console.log(err));
+  const findSwitches = () => {
+    return SwitchList.findAll({
+      'attributes': ['ip', 'community', 'uplink', 'subnet'],
+      'where': {
+        'dorm': req.params.dorm,
+        'level': -1
+      }
+    });
+  };
 
-  res
-    .set('Content-Type', 'Application/json')
-    .status(403)
-    .send('{}')
-    .end();
+  const getPortIndex = switchList => {
+    const findPort = Promise.all( switchList.map(e => getMACPortIndex(e.ip, e.community, req.body.chaddr)) );
+    return findPort.then(portIndex => {
+      let whichSwitch;
+      for (whichSwitch = 0; whichSwitch < portIndex.length; ++whichSwitch) {
+        if (portIndex[whichSwitch] !== switchList[whichSwitch].uplink) break;
+      }
+      return Promise.resolve([switchList[whichSwitch], portIndex[whichSwitch]]);
+    });
+  };
+
+  const getSwitchPort = (switchInfo, portIndex) => {
+    return switchInfo.getSwitchPort({
+      'attributes': ['dorm', 'room', 'plug'],
+      'where': { 'port': portIndex },
+      'raw': true
+    });
+  };
+
+  const getIp = data => {
+    if (data.length === 0) {
+      return Promise.reject(`cannot find switch port`);
+    }
+
+    const dvo = `${data[0].dorm}${data[0].room}-${data[0].plug}`;
+    return sql.query('SELECT `ip`, `dvo` FROM `ip_dvo` WHERE `dvo`=? OR `dvo`=? ORDER BY `dvo` DESC', {
+      'type': sequelize.QueryTypes.SELECT,
+      'replacements': [dvo, `${data[0].dorm}-gateway`]
+    }).then(data => {
+      if (data.length < 2) return Promise.reject(`cannot find ip of ${dvo}`);
+
+      const userIp = data.filter(e => e.dvo === dvo)[0];
+      const gateway = data.filter(e => e.dvo.match(/gateway/))[0];
+      if (!userIp || !gateway) return Promise.reject(`cannot find ip of ${dvo}`);
+
+      return Promise.resolve({'user': userIp.ip, 'gateway': gateway.ip});
+    });
+  };
+
+  const fillResponse = (ip, gateway, mask, lease) => {
+    const responseObject = {};
+    responseObject.yiaddr = ip;
+    responseObject.subnet_mask = mask;
+    responseObject.router = gateway;
+    responseObject.ip_address_lease_time = lease;
+    return responseObject;
+  };
+
+  const respond = resObj => {
+    res
+      .set('Content-Type', 'Application/json')
+      .status(200)
+      .send(JSON.stringify(resObj))
+      .end();
+  };
+
+  const switchList = findSwitches();
+  const portIndex = switchList.then(getPortIndex);
+  const switchPort = portIndex.then( data => getSwitchPort(data[0], data[1]) );
+  const ip = switchPort.then(getIp);
+
+  Promise.all([portIndex, ip]).then(data => {
+    const maskDecimal = parseInt(data[0][0].subnet.split('/')[1], 10);
+    const maskBinary = Array(maskDecimal).fill('1').concat(Array(32-maskDecimal).fill('0'));
+    const maskDotted = maskBinary.join('').match(/\d{8}/g).map(e => parseInt(e, 2)).join('.');
+    const userIp = data[1].user;
+    const gateway = data[1].gateway;
+    const lease = 14400; // 4 hours
+
+    const resObj = fillResponse(userIp, gateway, maskDotted, lease);
+
+    respond(resObj);
+  })
+    .catch(err => {
+      console.log(err);
+      res
+        .set('Content-Type', 'Application/json')
+        .status(500)
+        .send('{}')
+        .end();
+
+      return;
+    });
 });
 
 module.exports = router;
