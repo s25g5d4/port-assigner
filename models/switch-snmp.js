5022bfbd11e8f252331a901e241bb74a4a92dd9a
s25g5d4 <s25g5d4@gmail.com>
2016-11-13 23:02:06 +0800

    fix getMACPortIndex and add get/set ifAdminStatus

--- models/switch-snmp.js
+++ models/switch-snmp.js
@@ -1,9 +1,11 @@
 const snmp = require('net-snmp');
+const { decimalToMac } = require('../helpers/mac');
 
 const dot1dBasePortIfIndex = '1.3.6.1.2.1.17.1.4.1.2';
 const dot1qVlanStaticName = '1.3.6.1.2.1.17.7.1.4.3.1.1';
 const dot1qTpFdbPort = '1.3.6.1.2.1.17.7.1.2.2.1.2';
 const dot1dTpFdbPort = '1.3.6.1.2.1.17.4.3.1.2';
+const ifAdminStatus = '1.3.6.1.2.1.2.2.1.7';
 const snmpOptions = {
   'version': snmp.Version2c
 };
@@ -82,42 +84,43 @@ const getMACPortIndex = function getMACPortIndex(switchIp, community, mac) {
           else return resolve(varbinds[0].value);
         };
 
-        session.get([`${dot1dTpFdbPort}.${mac.join('.')}`], doneCallback);
+        session.get([ `${dot1dTpFdbPort}.${mac.join('.')}` ], doneCallback);
       });
     };
 
     const dot1qFindPort = function dot1qFindPort() {
       const session = snmp.createSession(switchIp, community, snmpOptions);
-      return getVlanIndex(switchIp, community).then(vlans => {
 
+      return getVlanIndex(switchIp, community).then(vlans => {
         const findPort = function findPort(vlan) {
           return new Promise(function (resolve, reject) {
             const doneCallback = function doneCallback(err, varbinds) {
               if (err) return reject(err);
-              session.close();
 
-              if (snmp.isVarbindError(varbinds[0])) return reject(snmp.varbindError(varbinds[0]));
-              else return resolve(varbinds[0].value);
+              return resolve(varbinds[0]);
             };
 
-            session.get([`${dot1qTpFdbPort}.${vlan.id}.${mac.join('.')}`], doneCallback);
+            session.get([ `${dot1qTpFdbPort}.${vlan.id}.${mac.join('.')}` ], doneCallback);
           });
         }
 
         return Promise.all(vlans.map(findPort)).then(varbinds => {
-          const result = varbinds.filter(e => !snmp.isVarbindError(e));
-          if (result.length === 0) {
+          session.close();
+          varbinds = varbinds.filter(e => !snmp.isVarbindError(e));
+
+          if (varbinds.length === 0) {
             return Promise.reject({
               'type': 'snmp not found',
               'data': {
                 'what': 'port index',
-                'mac': mac.map(e => `0${e.toString(16)}`.slice(-2)).join('')
+                'switchIp': switchIp,
+                'mac': decimalToMac(mac)
               },
-              'message': `cannot find port index of ${mac.map(e => `0${e.toString(16)}`.slice(-2)).join('')}`
+              'message': `cannot find port index of ${decimalToMac(mac)} on ${switchIp}`
             });
           }
           else {
-            return Promise.resolve(result[0].value);
+            return Promise.resolve(varbinds[0].value);
           }
         });
 
@@ -132,6 +135,94 @@ const getMACPortIndex = function getMACPortIndex(switchIp, community, mac) {
   });
 };
 
+const getAdminStatus = function getAdminStatus(switchIp, community, port) {
+  return new Promise(function (resolve, reject) {
+    const session = snmp.createSession(switchIp, community, snmpOptions);
+
+    const doneCallback = function doneCallback(err, varbinds) {
+      if (err) return reject(err);
+      session.close();
+
+      if (snmp.isVarbindError(varbinds[0])) {
+        return reject({
+              'type': 'snmp not found',
+              'data': {
+                'what': 'port status',
+                'port': port
+              },
+              'message': `cannot find port status of port ${port} on ${switchIp}`
+        });
+      }
+      else {
+        return resolve(varbinds[0].value);
+      }
+    };
+
+    session.get([ `${ifAdminStatus}.${port}` ], doneCallback);
+  });
+};
+
+const setAdminStatus = function setAdminStatus(switchIp, community, port, status) {
+  return new Promise(function (resolve, reject) {
+    const session = snmp.createSession(switchIp, community, snmpOptions);
+
+    const doneCallback = function doneCallback(err, varbinds) {
+      if (err) return reject(err);
+      session.close();
+
+      if (snmp.isVarbindError(varbinds[0])) {
+        return reject({
+              'type': 'snmp not found',
+              'data': {
+                'what': 'port status',
+                'port': port,
+                'status': status
+              },
+              'message': `cannot set port status of port ${port} on ${switchIp}`
+        });
+      }
+      else {
+        return resolve(varbinds[0].value);
+      }
+    };
+
+    if (typeof status === 'string') {
+      switch (status) {
+        case 'up':
+          status = 1;
+          break;
+
+        case 'down':
+          status = 2;
+          break;
+
+        case 'testing':
+          status = 3;
+          break;
+
+        default:
+          return reject(new TypeError(`invalid status: ${status}`));
+      }
+    }
+    else {
+      if (status !== 1 && status !== 2 && status !== 3) {
+        return reject(new TypeError(`invalid status: ${status}`));
+      }
+    }
+
+    session.set([
+      {
+        'oid': `${ifAdminStatus}.${port}`,
+        'type': snmp.ObjectType.Integer32,
+        'value': status
+      }
+    ], doneCallback);
+  });
+
+};
+
 exports.getSwitchPortIndex = getSwitchPortIndex;
 exports.getVlanIndex = getVlanIndex;
 exports.getMACPortIndex = getMACPortIndex;
+exports.getAdminStatus = getAdminStatus;
+exports.setAdminStatus = setAdminStatus;
